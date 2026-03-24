5922d94c2bd6a54a2163017d5c008f1c89bf7ccc
karol-Isabella <karol.cabrerap@campusucc.edu.co>
2026-03-24 18:45:21 -0500

    configuración aunth

--- src/main/java/com/proyectofinal/backendapi/config/SecurityConfig.java
+++ src/main/java/com/proyectofinal/backendapi/config/SecurityConfig.java
@@ -1,4 +1,24 @@
 package com.proyectofinal.backendapi.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.http.HttpMethod;
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .formLogin(form -> form.disable());
+
+        return http.build();
+    }
 }
--- src/main/java/com/proyectofinal/backendapi/service/UserService.java
+++ src/main/java/com/proyectofinal/backendapi/service/UserService.java
@@ -1,4 +1,41 @@
 package com.proyectofinal.backendapi.service;
 
+import com.proyectofinal.backendapi.model.User;
+import com.proyectofinal.backendapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // REGISTRO
+    public User register(User user) {
+
+        Optional<User> existing = userRepository.findByEmail(user.getEmail());
+
+        if (existing.isPresent()) {
+            throw new RuntimeException("El usuario ya existe");
+        }
+
+        return userRepository.save(user);
+    }
+
+    // LOGIN
+    public User login(String email, String password) {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+        if (!user.getPassword().equals(password)) {
+            throw new RuntimeException("Contraseña incorrecta");
+        }
+
+        return user;
+    }
 }
+
