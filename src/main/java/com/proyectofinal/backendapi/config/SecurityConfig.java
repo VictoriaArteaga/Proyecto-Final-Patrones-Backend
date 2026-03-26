a043965a3650d6823ba15d6fc1f335b40f278be0
karol-Isabella <karol.cabrerap@campusucc.edu.co>
2026-03-26 15:25:16 -0500

    configuración de seguridad para permitir endpoints

--- src/main/java/com/proyectofinal/backendapi/config/SecurityConfig.java
+++ src/main/java/com/proyectofinal/backendapi/config/SecurityConfig.java
@@ -4,20 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.http.HttpMethod;
+
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(httpBasic -> httpBasic.disable())
-                .formLogin(form -> form.disable());
+                        .anyRequest().permitAll()
+                );
 
         return http.build();
     }
