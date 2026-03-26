8fa0d97fb3e23d5580027dc56898321a74d8c854
karol-Isabella <karol.cabrerap@campusucc.edu.co>
2026-03-26 15:54:57 -0500

    se habilito cors y Api

--- src/main/java/com/proyectofinal/backendapi/config/SecurityConfig.java
+++ src/main/java/com/proyectofinal/backendapi/config/SecurityConfig.java
@@ -2,8 +2,12 @@ package com.proyectofinal.backendapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -12,10 +16,24 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> {})
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 );
 
         return http.build();
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 }
